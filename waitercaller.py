27decabe0cb92e1658f54b6636c1e8bf8dcec03f
Victor Ekwueme <victor.ekwueme@gmail.com>
2018-02-16 13:15:53 +0100

    initial db config

--- waitercaller.py
+++ waitercaller.py
@@ -1,10 +1,22 @@
 from flask import Flask
+from flask.ext.login import LoginManager
+from flask.ext.login import login_required
+from mockdbhelper import MockDBHelper as DBHelper
+from user import User
 
+DB = DBHelper()
 app = Flask(__name__)
+login_manager = LoginManager(app)
 
 @app.route('/')
 def home():
-    return 'Under Construction'
+    return render_template('home.html')
+
+@app.route("/account")
+@login_required
+def account():
+    return "You are logged in"
+
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
