f0479371a3145ec264c2c5da7e0d0540319f5d64
Peter Ahueze <ahuezepeter25@gmail.com>
2018-03-12 11:57:19 +0000

    cascading templates 1

--- waitercaller.py
+++ waitercaller.py
@@ -1,10 +1,14 @@
-from flask import Flask
-from flask.ext.login import LoginManager
-from flask.ext.login import login_required
+from flask import Flask, request, url_for, render_template, url_for
+from flask_login import LoginManager, login_user
+from flask_login import login_required
 from mockdbhelper import MockDBHelper as DBHelper
 from user import User
+from passwordhelper import PasswordHelper
+
 
 DB = DBHelper()
+PH = PasswordHelper()
+
 app = Flask(__name__)
 
 app.secret_key = 'i2QvD4cd4aAPjCampxPkjWliR0znwS9Y3ZC41jmPBlAFzBfiNf1mo0q9G/Iw0GtwCoxSjDz20Bdp4hICXs1RX5KvPwTDdZgrcHW'
@@ -20,8 +24,47 @@ def home():
 def account():
     return "You are logged in"
 
+@app.route("/login", methods=["POST"])
+def login():
+    email = request.form.get("email")
+    password = request.form.get("password")
+    stored_user = DB.get_user(email)
+    if stored_user and PH.validate_password(password,
+      stored_user['salt'], stored_user['hashed']):
+        user = User(email)
+        login_user(user, remember=True)
+        return redirect(url_for('account'))
+    return home()
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    user_password = DB.get_user(user_id)
+    if user_password:
+        return User(user_id)
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for("home"))
+
+@app.route("/register", methods=["POST"])
+def register():
+    email = request.form.get("email")
+    pw1 = request.form.get("password")
+    pw2 = request.form.get("password2")
+    if not pw1 == pw2:
+        return redirect(url_for('home'))
+    if DB.get_user(email):
+        return redirect(url_for('home'))
+    salt = PH.get_salt()
+    hashed = PH.get_hash(pw1 + salt)
+    DB.add_user(email, salt, hashed)
+    return redirect(url_for('home'))
+
 
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    import os
+    host = os.environ.get('IP','82.196.12.230')
+    app.run(port=5000, debug=True,host=host)
 
 
